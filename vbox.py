bcdc69835f6eeecbe5e8c3967ed84ce7757095b6
wooc82 <greg.sli@gmail.com>
2020-01-08 17:44:11 -0500

    second menu item and fixed missing last line in the command output capture

--- vbox.py
+++ vbox.py
@@ -7,14 +7,13 @@ import re
 
 #virtualbox basic commands
 list_vms_cmd = 'vboxmanage list vms --sorted'
-list_running_cmd = 'vboxmanage list runningvms'
+list_running_cmd = 'vboxmanage list runningvms --sorted'
 list_vmdetails_cmd = 'vboxmanage guestproperty enumerate '
 
 
 vm_list = []
+running_vm_list = []
 vm = 'Ubuntu_3'
-parameter = '-l'
-vmname = '/home/wooc'
 lsl = list_vmdetails_cmd + vm
 IP_addr = ""
 onoff = ""
@@ -26,15 +25,33 @@ class vboxvm:
         self.status = onoff
 
 
-
+#list all running VMs configured in Virtualbox
 def vmlist():
+    #using global variable and clearing the list
     global vm_list
     vm_list = []
+    #loading list_vms_cmd output to the list
     vmlist_output = output_to_lines(list_vms_cmd)
+    #extracting hostnames and adding them to the vm_list list
     for line_number in range(len(vmlist_output)):
         line_content = vmlist_output[line_number]
         capture_name = re.findall(r'"(.*?)"',line_content)
         vm_list.append(capture_name[0])
+    return vm_list
+
+
+#list all VMs configured in Virtualbox
+def runningvmlist():
+    #using global variable and clearing the list
+    global running_vm_list
+    running_vm_list = []
+    #loading list_vms_cmd output to the list
+    running_vm_list_output = output_to_lines(list_running_cmd)
+    #extracting hostnames and adding them to the vm_list list
+    for line_number in range(len(running_vm_list_output)):
+        line_content = running_vm_list_output[line_number]
+        capture_name = re.findall(r'"(.*?)"',line_content)
+        running_vm_list.append(capture_name[0])
     return vm_list
 
 
@@ -47,19 +64,19 @@ def get_ip(vm_name):
             IP_addr = line_content.split()[3][:-1]
         else:
             IP_addr = "Check g additions"
-
     return IP_addr
 
-print(IP_addr)
 
-
+#function that can run any command and put output in to list one line per list item
 def output_to_lines(command):
-#result = subprocess.run([vm, parameter, vmname], stdout=subprocess.PIPE).stdout.decode('utf-8')
     result = subprocess.getoutput(command)
+    length = len(result)
+    possition = 0
     output_lines = []
     line_content = ''
     for char in result:
-        if char != '\n':
+        possition = possition + 1
+        if char != '\n' and possition != length-1:
             line_content = line_content + str(char)
         else:
             output_lines.append(line_content)
@@ -69,10 +86,14 @@ def output_to_lines(command):
 
 def list_all_vms():
     vmlist()
-    #print("List of all VMs:")
     for vmnumber in range(len(vm_list)):
         print(vmnumber, "\t", vm_list[vmnumber])
 
+
+def list_running_vms():
+    runningvmlist()
+    for vmnumber in range(len(running_vm_list)):
+        print(vmnumber, "\t", running_vm_list[vmnumber])
 
 
 def main_menu():
@@ -93,6 +114,11 @@ def main_menu():
         list_all_vms()
         input("\nPress enter to go back to main menu...")
         main_menu()
+    if option == "2":
+        list_running_vms()
+        input("\nPress enter to go back to main menu...")
+        main_menu()
+
     if option == "q":
         quit()
     else:
