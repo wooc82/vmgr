bc370157583e8944d0e319284670d65e1d482f7e
wooc82 <greg.sli@gmail.com>
2020-01-08 12:01:30 -0500

    made command to list a function

--- vbox.py
+++ vbox.py
@@ -1,4 +1,3 @@
-import vboxapi
 import subprocess
 
 #result = subprocess.run(['ls', '-l', '/home/wooc'], stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -8,26 +7,32 @@ import subprocess
 vm = 'ls -l '
 parameter = '-l'
 vmname = '/home/wooc'
-command = vm + vmname
+lsl = vm + vmname
 
+def output_to_lines(command):
 #result = subprocess.run([vm, parameter, vmname], stdout=subprocess.PIPE).stdout.decode('utf-8')
-result = subprocess.getoutput(command)
+    result = subprocess.getoutput(command)
 
 
-output_lines = []
-line_content = ''
-for char in result:
-    if char != '\n':
-        line_content = line_content + str(char)
-    else:
-        output_lines.append(line_content)
-        line_content = ''
+    output_lines = []
+    line_content = ''
+    for char in result:
+        if char != '\n':
+            line_content = line_content + str(char)
+        else:
+            output_lines.append(line_content)
+            line_content = ''
 
 
-for line_nr in range (0,len(output_lines)):
-    print(output_lines[line_nr])
+    for line_nr in range (0,len(output_lines)):
+        print(output_lines[line_nr])
 
+
+
+print(output_to_lines(lsl))
 print('\ttest\t')
+
+
 
 
 #print(result)
